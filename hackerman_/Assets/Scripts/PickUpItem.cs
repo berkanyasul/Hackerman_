b0a51bdb70453e628b9b2285938e6b8abca6e7bf
Yasul, Berkan <s65891@beuth-hochschule.de>
2017-06-01 11:34:29 +0200

    Camera follow & basic Item Pickup Logic!

---
+++ hackerman_/Assets/Scripts/PickUpItem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpItem : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		GameObject g = other.gameObject;
+		Debug.Log (g.name);
+		if (g.CompareTag("Player")) {
+			Destroy (gameObject);
+		}
+	}
+}
