c678bf52a5292ea1adabef687c21a347683e1395
Yasul, Berkan <s65891@beuth-hochschule.de>
2017-06-06 23:42:27 +0200

    Added GameOver Logic when pickung up all the given items, now need enemy AI

--- hackerman_/Assets/Scripts/PickUpItem.cs
+++ hackerman_/Assets/Scripts/PickUpItem.cs
@@ -1,23 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class PickUpItem : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+		gameOver = GameObject.FindGameObjectWithTag ("Player").GetComponent<GameOverController> ();	
 	}
-	
+
+	GameOverController gameOver;
+
 	// Update is called once per frame
 	void Update () {
-		
+	
+	
 	}
 
 	void OnTriggerEnter2D(Collider2D other){
 		GameObject g = other.gameObject;
-		Debug.Log (g.name);
 		if (g.CompareTag("Player")) {
+			gameOver.pickedUpItem ();
 			Destroy (gameObject);
 		}
 	}
