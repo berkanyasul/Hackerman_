72e07bc5fdb76d6191208333a21e9b16179bb4a0
Yasul, Berkan <s65891@beuth-hochschule.de>
2017-06-18 12:58:54 +0200

    Patrolling Enemy skrr skrr skrr

--- hackerman_/Assets/Scripts/EnemyController.cs
+++ hackerman_/Assets/Scripts/EnemyController.cs
@@ -6,24 +6,36 @@ public class EnemyController : MonoBehaviour {
 
 	private GameObject Player;
 	private Rigidbody2D rb;
-	public GameObject patrolPoints;
+	public GameObject patrolPoint;
 	//public Transform Player;
 
 	// how fast we want the enemy to chase
 	public float ChaseSpeed = 5f;
-
+	public float PatrolSpeed = 3f;
 	// the range at which it detects Player
 	public float Range = 5f;
 	public float GameOverRange = 1f;
 	// what our current speed is (get only)
 	float CurrentSpeed;
+	float WaitTimer = 0f;
+	float MaxTimer = 2f;
 
+	Transform[] patrolPoints;
+	Transform currentPoint;
+	int currentCount = 0;
 	// Use this for initialization
 	void Start () {
 		Player = GameObject.FindGameObjectWithTag ("Player");
-		patrolPoints = GameObject.FindGameObjectWithTag ("PatrolPoints");
-		Debug.Log ("___");
-		Debug.Log (patrolPoints.ToString());
+		patrolPoint = GameObject.FindGameObjectWithTag ("PatrolPoints");
+		patrolPoints = new Transform[patrolPoint.transform.childCount];
+
+		var i = 0;
+		foreach(Transform child in patrolPoint.transform)
+		{
+			patrolPoints[i] = (child);
+			i++;
+		}
+		currentPoint = patrolPoints[currentCount];
 		rb = GetComponent<Rigidbody2D>();		
 	}
 	
@@ -41,7 +53,32 @@ public class EnemyController : MonoBehaviour {
 			//rb.AddForce(test, ForceMode2D.Force);
 		} else if (dist < GameOverRange) {
 			Player.GetComponent<GameOverController> ().gameOver ();
+		} else {
+			patrol();
 		}	
+	}
+
+	void patrol(){
+		float patrolDist = Vector3.Distance(transform.position,currentPoint.position);
+		if (patrolDist <= 0.1f) {
+			changeCurrentPoint ();
+		}
+		Vector3 direction = (currentPoint.position - transform.position).normalized;
+		rb.MovePosition (transform.position + direction * PatrolSpeed * Time.deltaTime);
+	}
+
+	void changeCurrentPoint(){
+		WaitTimer += Time.deltaTime;
+		if (WaitTimer > MaxTimer) {
+			currentCount++;
+			if (currentCount == patrolPoint.transform.childCount) {
+				currentCount = 0;
+			}
+			currentPoint = patrolPoints[currentCount];
+			WaitTimer = 0f;
+		}
+
+
 	}
 }
 
